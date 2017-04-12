6619b24f86b2cd2b7760ecb518bf2f2254bc93af
quophyie <quophyie@yahoo.com>
2017-04-12 17:19:11 +0100

    Moved TestUtil to com.quantal.basecomponents.util

--- src/main/java/com/quantal/basecomponents/util/TestUtil.java
+++ src/main/java/com/quantal/basecomponents/util/TestUtil.java
@@ -1,4 +1,4 @@
-package com.quantal.util;
+package com.quantal.basecomponents.util;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quantal.basecomponents.dto.ResponseDto;
