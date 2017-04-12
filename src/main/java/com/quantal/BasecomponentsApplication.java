f1d526e86edb049c72e98962e9dd3f836253e873
quophyie <quophyie@yahoo.com>
2017-04-12 16:01:06 +0100

    Removed code that makes the base components a spring boot app i.e. @SpringBootApplication
    Also Stopped the Spring Boot Maven plugin from running

--- src/main/java/com/quantal/BasecomponentsApplication.java
+++ src/main/java/com/quantal/BasecomponentsApplication.java
@@ -3,10 +3,11 @@ package com.quantal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/*@SpringBootApplication
 public class BasecomponentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasecomponentsApplication.class, args);
 	}
 }
+*/
