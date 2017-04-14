06d59ddf10b833bccb24160df14a9327793af516
quophyie <quophyie@yahoo.com>
2017-04-14 15:00:42 +0100

    Added exceptions

---
+++ src/main/java/com/quantal/shared/exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.quantal.shared.exceptions;
+
+/**
+ * Created by dman on 29/03/2017.
+ */
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message){
+        super(message);
+    }
+}
