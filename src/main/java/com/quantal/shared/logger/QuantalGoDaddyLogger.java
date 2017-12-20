609c26ce0376689c39078313bc87f1a9b4f63545
quophyie <quophyie@yahoo.com>
2017-12-20 01:10:03 +0000

    updated throwing to return runtime exception

--- src/main/java/com/quantal/shared/logger/QuantalGoDaddyLogger.java
+++ src/main/java/com/quantal/shared/logger/QuantalGoDaddyLogger.java
@@ -4,7 +4,7 @@ import com.godaddy.logging.Logger;
 
 public interface QuantalGoDaddyLogger extends Logger {
 
-    default Throwable throwing(Throwable t) {
+    default RuntimeException throwing(Throwable t) {
         this.error(t.getMessage(),t);
           return new RuntimeException(t);
     }
