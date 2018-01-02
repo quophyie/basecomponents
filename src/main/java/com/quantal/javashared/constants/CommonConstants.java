46b122f9f8828d1c55157759bf644ab1e1f7bd11
quophyie <quophyie@yahoo.com>
2018-01-02 23:32:30 +0000

    Added some more common constants

--- src/main/java/com/quantal/javashared/constants/CommonConstants.java
+++ src/main/java/com/quantal/javashared/constants/CommonConstants.java
@@ -7,6 +7,7 @@ public class CommonConstants {
     public static final String TRACE_ID_HEADER_KEY = "X-TraceId";
     public static final String TRACE_ID_MDC_KEY = "traceId";
     public static final String USER_KEY = "user";
+    public static final String USER_ID_KEY = "userId";
     public static final String EMAIL_KEY = "email";
     public static final String REQUEST_KEY = "request";
     public static final String RESPONSE_KEY = "request";
