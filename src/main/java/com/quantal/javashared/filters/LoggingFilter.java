104a8787df490453cb21f2ebb63aa1198cb4457f
quophyie <quophyie@yahoo.com>
2018-03-19 00:23:39 +0000

    Updated Logging Filter

--- src/main/java/com/quantal/javashared/filters/LoggingFilter.java
+++ src/main/java/com/quantal/javashared/filters/LoggingFilter.java
@@ -5,17 +5,17 @@ import com.quantal.javashared.dto.LogEvent;
 import com.quantal.javashared.logger.QuantalLogger;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.ThreadContext;
-import org.slf4j.MDC;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.web.filter.GenericFilterBean;
 
 import static com.quantal.javashared.constants.CommonConstants.EVENT_HEADER_KEY;
 import static com.quantal.javashared.constants.CommonConstants.EVENT_KEY;
@@ -28,6 +28,9 @@ import static com.quantal.javashared.constants.CommonConstants.TRACE_ID_MDC_KEY;
  * Created by dman on 08/07/2017.
  */
 
+
+//@Component
+//@Configurable
 public class LoggingFilter extends GenericFilterBean {
 
 
@@ -38,30 +41,28 @@ public class LoggingFilter extends GenericFilterBean {
     @InjectLogger
     private QuantalLogger logger;
 
+    public String generateTraceId(){
+        return  UUID.randomUUID().toString();
+    }
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
 
         try {
-            String traceId = ((HttpServletRequest) request).getHeader(TRACE_ID_HEADER_KEY);
-            String event = ((HttpServletRequest) request).getHeader(EVENT_HEADER_KEY);
+            String traceId = !StringUtils.isEmpty(((HttpServletRequest) request).getHeader(TRACE_ID_HEADER_KEY)) ? ((HttpServletRequest) request).getHeader(TRACE_ID_HEADER_KEY) : generateTraceId();
+            String event = !StringUtils.isEmpty(((HttpServletRequest) request).getHeader(EVENT_HEADER_KEY)) ? ((HttpServletRequest) request).getHeader(EVENT_HEADER_KEY) : "REQUEST_RECEIVED";
 
-
-            if (!StringUtils.isEmpty(traceId)) {
-                MDC.put(TRACE_ID_MDC_KEY, traceId);
-                ThreadContext.put(TRACE_ID_MDC_KEY, traceId);
-            } else {
-                MDC.put(TRACE_ID_MDC_KEY, UUID.randomUUID().toString());
-                ThreadContext.put(TRACE_ID_MDC_KEY, UUID.randomUUID().toString());
+            String msg = "request received successfully";
+            if(!StringUtils.isEmpty(((HttpServletRequest) request).getHeader(EVENT_HEADER_KEY))){
+                msg = String.format("progressing %", ((HttpServletRequest) request).getHeader(EVENT_HEADER_KEY));
             }
 
-            if (!StringUtils.isEmpty(event)) {
-                MDC.put(EVENT_KEY, event);
-                ThreadContext.put(EVENT_KEY, event);
-            }
 
-            logger.with(REQUEST_KEY,request.toString())
-                  .with(EVENT_KEY, "REQUEST_RECEIVED")
-                  .info("request received successfully");
+            MDC.put(TRACE_ID_MDC_KEY, traceId);
+            MDC.put(EVENT_HEADER_KEY, event);
+
+            logger.with(REQUEST_KEY, request.toString())
+                    .with(EVENT_KEY, event)
+                    .info(msg);
             chain.doFilter(request, response);
             traceId = ((HttpServletResponse) response).getHeader(TRACE_ID_HEADER_KEY);
             if (StringUtils.isEmpty(traceId)) {
@@ -71,11 +72,16 @@ public class LoggingFilter extends GenericFilterBean {
             logger.with(RESPONSE_KEY, response.toString()).debug("response sent successfully ", new LogEvent("RESPONSE_SENT"));
         } finally {
             ThreadContext.remove(TRACE_ID_MDC_KEY);
+            MDC.remove(TRACE_ID_HEADER_KEY);
+            ThreadContext.remove(EVENT_KEY);
+            MDC.remove(EVENT_HEADER_KEY);
         }
     }
 
     @Override
     public void destroy() {
-        logger.with(EVENT_KEY, "FILTER_DESTROY").warn("Destroying LoggingFilter ...");
+        logger.with(EVENT_KEY, "FILTER_DESTROY")
+                .with(TRACE_ID_MDC_KEY, generateTraceId())
+                .warn("Destroying LoggingFilter ...");
     }
 }
