cad366d4aa2bfcc07a8da781eb5cfc2e45472051
quophyie <quophyie@yahoo.com>
2018-03-28 11:16:44 +0100

    updated pointcut expression in RetrofitRequiredHeadersEnforcerAspect to  catch classes implementing api services

--- src/main/java/com/quantal/javashared/aspects/RetrofitRequiredHeadersEnforcerAspect.java
+++ src/main/java/com/quantal/javashared/aspects/RetrofitRequiredHeadersEnforcerAspect.java
@@ -120,7 +120,7 @@ public class RetrofitRequiredHeadersEnforcerAspect {
             "|| @annotation(com.quantal.javashared.annotations.requestheaders.EnforceRequiredHeaders)" +
 
             ")")*/
-    @Pointcut("target(com.quantal..services.api.*Service+.*) &&" +
+    @Pointcut("(execution(* com.quantal..services.api.*ervice+.*.*(..))  &&" +
             "(@target(com.quantal.javashared.annotations.requestheaders.EnforceRequiredHeaders)" +
             "|| @within(com.quantal.javashared.annotations.requestheaders.EnforceRequiredHeaders)" +
             "|| @annotation(com.quantal.javashared.annotations.requestheaders.EnforceRequiredHeaders))")
